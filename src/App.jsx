6be9b4a4e9e9b7e24ccf8f242ea272257777847f
David Arrizon <arrizon.david@gmail.com>
2025-12-23 20:51:35 -0800

    Add main entry files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ErrorBoundary } from './components/ErrorBoundary';
+import { InspectionForm } from './components/InspectionForm';
+
+export default function App() {
+  return (
+    <ErrorBoundary>
+      <InspectionForm />
+    </ErrorBoundary>
+  );
+}
